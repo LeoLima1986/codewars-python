43fc9f3ba3f242521e0a862bfbc37d55a4ace2bd
LeoLima1986 <59488723+LeoLima1986@users.noreply.github.com>
2020-05-29 12:23:16 -0500

    Create anagrams.py

---
+++ anagrams.py
@@ -0,0 +1,23 @@
+'''
+Problem Description: What is an anagram? Well, two words are anagrams of each other if they both contain the same letters. For example:
+'abba' & 'baab' == true
+'abba' & 'bbaa' == true
+'abba' & 'abbba' == false
+Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. 
+'''
+
+def anagrams(word, words):
+  newlist = []
+  word_sort = sorted(word)
+  print(word_sort)
+  print(words)
+  for i in words:
+    print(sorted(i))
+    if sorted(i) == word_sort:
+      newlist.append(i)
+  return newlist
+  
+  # Test Cases
+
+print(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']))
+print(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']))
